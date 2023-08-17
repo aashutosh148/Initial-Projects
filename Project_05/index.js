9b62ea0171f8b45a5a87be34ddc95605c3a4c5b5
aashutosh148 <aashutoshsingh468847@gmail.com>
2023-08-17 19:44:37 +0530

    completed day5 work

---
+++ Project_05/index.js
@@ -0,0 +1,22 @@
+const load = document.querySelector('.loading-text');
+const bg = document.querySelector('img');
+let loading = 0;
+
+const int = setInterval(() => {
+    loading++;
+    if(loading === 100)clearInterval(int);
+
+    update();
+}, 30);
+
+function update() 
+{
+    load.textContent = loading + "%";
+    load.style.opacity = scale(loading, 0, 100, 1, 0);
+    bg.style.filter = `blur(${scale(loading, 0, 100, 30, 0)}px)`;
+}
+
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
